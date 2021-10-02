fe3a0c773135542cdfb05840d1cb4662b386e285
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2021-10-02 13:32:18 -0500

    Create Main.php

---
+++ src/Creadores/Vasiar/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Creadores\Vasiar;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginManager;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\player\Player;
+use pocketmine\player\Server;
+use pocketmine\item\Item;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(){
+        $this->getLogger()->info("Cargando...");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("Cargado!");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        switch($command->getName()){
+            case "ví":
+                $sender->sendMessage("§b[§aCreadores§b]§a Has vasiado tu inventario!");
+                $sender->getPlayer()->getinventory()->clearALL();
